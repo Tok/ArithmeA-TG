4df353373d7e42feb9905e4002f7fafd0a159d8d
Tok <lsteiger4@gmail.com>
2022-08-26 18:22:50 +0200

    Dependencies updated.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject arithmea "0.7.7-SNAPSHOT"
   :description "ArithmeA Telegram Bot"
   :url "https://github.com/Tok/ArithmeA-TG"
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]]
                    :resource-paths ["resources-test"]}}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/tools.logging "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/tools.logging "1.2.1"]
                  [clj-http "3.10.0"]]
   :aot [arithmea.core]
   :main arithmea.core)
